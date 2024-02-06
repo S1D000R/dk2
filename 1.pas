e4b2d980429dd18820d44d2cf410ffbd20fedfdf
Maxim Isupov <124869886+S1D000R@users.noreply.github.com>
2024-02-06 17:50:22 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,23 @@
+﻿﻿Program Dkr22;
+var str,str1,print:string; i,k:integer;
+begin
+i := 1;
+k := 1;
+print := 'Строки совпадают';
+writeln ('Введите исходную строку');
+readln (str);
+writeln ('Введите строку');
+readln (str1);
+if length(str) = length(str1) then begin 
+  while (i <= length(str)) and (i <= length(str1)) do begin
+    if str[i] = str1[i] then begin
+      i := i + 1;
+    end
+    else begin
+      print := 'Строки не совпадают';
+      break
+      end;
+  end;
+end;
+writeln (print);
+end.
---
+++ 2.pas
@@ -0,0 +1,20 @@
+﻿Program Dkr21;
+var i,k,max:integer; A:array[1..10] of integer;
+begin
+max := 0;
+Writeln ('Вводите элемент массив (по одному)');
+for i := 1 to 10 do begin
+  read (A[i]);
+end;
+writeln (A);
+for i:= 1 to 10 do begin
+  if A[i] > max then
+    max := A[i];
+end;
+for i:= 1 to 10 do begin
+  if A[i] = max then
+    k := k + 1;
+end;
+writeln ('Максимальный элемент: ', max);
+writeln ('Количество вхождений в массив: ', k);
+end.
